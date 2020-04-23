a86055ca0cb74390f7d996574e9cccfbc989fbe3
aravinthsivarasa <aravinth2301@gmail.com>
2020-04-23 13:57:37 +1000

    added dashboard page

---
+++ gui/vue/src/components/DashboardCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="srcImg" alt="Placeholder image" />
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img :src="iconImg" alt="Placeholder image" />
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{ title }}</p>
+          <p class="subtitle is-6">{{ description }}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        {{ content }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    srcImg: {
+      type: String,
+      default: null
+    },
+    iconImg: {
+      type: String,
+      default: null
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    description: {
+      type: String,
+      default: null
+    },
+    content: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
--- gui/vue/src/router/index.js
+++ gui/vue/src/router/index.js
@@ -18,6 +18,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    component: () => import(/* webpackChunkName: "login" */ "../Login.vue")
+  },
+  {
+    path: "/dashboard",
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "../Dashboard.vue")
   }
 ];
 

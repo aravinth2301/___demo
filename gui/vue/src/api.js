1eb90512c273027926c4244c15067cda423cbb9b
aravinthsivarasa <aravinth2301@gmail.com>
2020-05-02 13:49:09 +1000

    configured to https

--- gui/vue/src/api.js
+++ gui/vue/src/api.js
@@ -9,7 +9,7 @@ export const vappApi = axios.create({
 
 export const servApi = axios.create({
   // baseURL: "http://localhost:9000/service-node/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://localhost:8443/api",
   headers: {
     "Content-type": "application/json",
     Authorization: "Basic YWRtaW46YWRtaW4="

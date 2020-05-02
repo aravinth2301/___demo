89e572f7bb16839cc184821fee0321a0ac73f3a6
aravinthsivarasa <aravinth2301@gmail.com>
2020-05-02 10:33:29 +1000

    added SSL config

--- __modules/__sample/sample/src/main/java/au/company/product/SampleMain.java
+++ __modules/__sample/sample/src/main/java/au/company/product/SampleMain.java
@@ -3,13 +3,12 @@ package au.company.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SampleMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(SampleMain.class,args);
+        System.setProperty("spring.config.name", "sample,application");
+        SpringApplication.run(SampleMain.class, args);
     }
 }
--- _lib-service/src/main/java/au/company/product/lib/service/config/EurekaConfig.java
+++ _lib-service/src/main/java/au/company/product/lib/service/config/EurekaConfig.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile(value = {"docker","default"})
+@Profile(value = {"docker","local-eureka"})
 @Configuration
 @EnableDiscoveryClient
 public class EurekaConfig {
